ed17b4488df4a4df3efda78b592bf5321030e356
chaidenfoanto <chaiden.foanto@gmail.com>
2025-01-15 00:28:45 +0800

    booking

--- lib/booking.dart
+++ lib/booking.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'package:front_end/widgets/map_service.dart';
+import 'package:front_end/widgets/service_options.dart';
 import 'package:front_end/widgets/custom_map_widget.dart';
+import 'package:image_picker/image_picker.dart';
 
 class BookingScreen extends StatefulWidget {
   const BookingScreen({super.key});
@@ -19,6 +23,10 @@ class _BookingScreenState extends State<BookingScreen>
   late TabController _tabController;
   String selectedService = '';
   final PanelController _panelController = PanelController();
+  final ValueNotifier<double> panelPositionNotifier = ValueNotifier(0);
+  final ImagePicker _picker = ImagePicker();
+  List<XFile>? _images = [];  // Menyimpan daftar gambar
+  List<XFile>? _videos = [];  // Menyimpan daftar video
 
   @override
   void initState() {
@@ -40,6 +48,56 @@ class _BookingScreenState extends State<BookingScreen>
         hintText = "Gagal mendapatkan lokasi.";
       });
     }
+  }
+
+  // Fungsi untuk memilih gambar atau video
+  Future<void> _pickMedia(ImageSource source, {required bool isCamera}) async {
+    final pickedFile = isCamera
+        ? await _picker.pickVideo(source: source) // Mengambil video dari kamera
+        : await _picker.pickImage(source: source); // Mengambil gambar dari kamera
+
+    if (pickedFile != null) {
+      setState(() {
+        if (pickedFile.path.endsWith('.mp4')) {
+          _videos!.add(pickedFile); // Menambahkan video
+        } else {
+          _images!.add(pickedFile); // Menambahkan gambar
+        }
+      });
+    }
+  }
+
+  // Fungsi untuk memilih sumber media
+  Future<void> _showMediaPickerDialog() async {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Pilih Sumber Media"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                title: const Text("Ambil Foto / Video"),
+                leading: const Icon(Icons.camera_alt),
+                onTap: () {
+                  Navigator.pop(context);
+                  _pickMedia(ImageSource.camera, isCamera: true); // Ambil dari kamera
+                },
+              ),
+              ListTile(
+                title: const Text("Pilih dari Galeri"),
+                leading: const Icon(Icons.photo),
+                onTap: () {
+                  Navigator.pop(context);
+                  _pickMedia(ImageSource.gallery, isCamera: false); // Ambil dari galeri
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 
   @override
@@ -88,45 +146,62 @@ class _BookingScreenState extends State<BookingScreen>
             ),
           ),
 
-          // Tombol Back
-          Positioned(
-            left: 16,
-            bottom: MediaQuery.of(context).size.height * 0.65 + 32,
-            child: CircleAvatar(
-              backgroundColor: Colors.white,
-              child: IconButton(
-                icon: const Icon(Icons.arrow_back, color: Colors.black),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ),
+          // Tombol Back yang dinamis
+          AnimatedBuilder(
+            animation: panelPositionNotifier,
+            builder: (context, child) {
+              final double maxPanelHeight =
+                  MediaQuery.of(context).size.height * 0.65;
+              final double minPanelHeight = 60;
+              final double panelHeight = panelPositionNotifier.value *
+                      (maxPanelHeight - minPanelHeight) +
+                  minPanelHeight;
+              final double backButtonPosition = panelHeight + 16;
+
+              return Positioned(
+                left: 16,
+                bottom: backButtonPosition,
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: IconButton(
+                    icon: const Icon(Icons.arrow_back, color: Colors.black),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ),
+              );
+            },
           ),
 
-          // Panel Sliding
           SlidingUpPanel(
             controller: _panelController,
-            minHeight: 60, // Tinggi minimum agar tab terlihat
+            minHeight: 60,
             maxHeight: MediaQuery.of(context).size.height * 0.65,
             borderRadius: const BorderRadius.only(
               topLeft: Radius.circular(20),
               topRight: Radius.circular(20),
             ),
+            onPanelSlide: (position) {
+              setState(() {
+                panelPositionNotifier.value = position;
+              });
+            },
             panelBuilder: (scrollController) => _buildSlidingPanel(
               theme,
               scrollController,
             ),
-            body: Container(), // Tetap kosong karena peta ada di belakang
+            body: Container(),
           ),
         ],
       ),
     );
   }
 
-  Widget _buildSlidingPanel(ThemeData theme, ScrollController scrollController) {
+  Widget _buildSlidingPanel(
+      ThemeData theme, ScrollController scrollController) {
     return Column(
       children: [
-        // Tab Bar
         TabBar(
           labelStyle: theme.textTheme.labelLarge?.copyWith(
             fontWeight: FontWeight.bold,
@@ -141,16 +216,12 @@ class _BookingScreenState extends State<BookingScreen>
             Tab(text: "Book first"),
           ],
         ),
-
         // Isi Konten Tab Bar
         Expanded(
           child: TabBarView(
             controller: _tabController,
             children: [
-              // Tab "Order now"
               _buildOrderNowContent(theme),
-
-              // Tab "Book first"
               _buildBookFirstContent(theme),
             ],
           ),
@@ -171,32 +242,19 @@ class _BookingScreenState extends State<BookingScreen>
                 .copyWith(fontWeight: FontWeight.bold),
           ),
           const SizedBox(height: 16),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              _serviceOption(
-                context,
-                icon: Icons.ac_unit, // Placeholder
-                label: "Recharge Freon",
-                isSelected: selectedService == "Recharge Freon",
-                onTap: () {
-                  setState(() {
-                    selectedService = "Recharge Freon";
-                  });
-                },
-              ),
-              _serviceOption(
-                context,
-                icon: Icons.cleaning_services, // Placeholder
-                label: "Cleaning Up",
-                isSelected: selectedService == "Cleaning Up",
-                onTap: () {
-                  setState(() {
-                    selectedService = "Cleaning Up";
-                  });
-                },
-              ),
-            ],
+          ServiceOptions(
+            selectedService: selectedService,
+            onServiceSelected: (service) {
+              setState(() {
+                selectedService = service;
+              });
+            },
+          ),
+          const SizedBox(height: 32),
+          Text(
+            "Order now for an instant pick..",
+            style: theme.textTheme.bodyMedium,
+            textAlign: TextAlign.center,
           ),
           const Spacer(),
           _buildCostAndOrderButton(theme),
@@ -220,32 +278,13 @@ class _BookingScreenState extends State<BookingScreen>
               ),
             ),
             const SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                _serviceOption(
-                  context,
-                  icon: Icons.ac_unit, // Placeholder
-                  label: "Recharge Freon",
-                  isSelected: selectedService == "Recharge Freon",
-                  onTap: () {
-                    setState(() {
-                      selectedService = "Recharge Freon";
-                    });
-                  },
-                ),
-                _serviceOption(
-                  context,
-                  icon: Icons.cleaning_services, // Placeholder
-                  label: "Cleaning Up",
-                  isSelected: selectedService == "Cleaning Up",
-                  onTap: () {
-                    setState(() {
-                      selectedService = "Cleaning Up";
-                    });
-                  },
-                ),
-              ],
+            ServiceOptions(
+              selectedService: selectedService,
+              onServiceSelected: (service) {
+                setState(() {
+                  selectedService = service;
+                });
+              },
             ),
             const SizedBox(height: 32),
             Text(
@@ -256,10 +295,9 @@ class _BookingScreenState extends State<BookingScreen>
             const SizedBox(height: 16),
             TextField(
               decoration: InputDecoration(
-                hintText: "Describe your issue...",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15),
-                ),
+                hintText: "Type here to let the technician know your problem.",
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
               ),
               maxLines: 3,
             ),
@@ -272,7 +310,10 @@ class _BookingScreenState extends State<BookingScreen>
             const SizedBox(height: 16),
             ElevatedButton(
               onPressed: () {},
-              child: const Text("Choose a Date"),
+              child: Text(
+                "Choose a Date",
+                style: theme.textTheme.bodySmall,
+              ),
             ),
             const SizedBox(height: 32),
             Center(
@@ -287,17 +328,25 @@ class _BookingScreenState extends State<BookingScreen>
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 ElevatedButton.icon(
-                  onPressed: () {},
-                  icon: const Icon(Icons.photo),
-                  label: const Text("Gallery"),
+                  onPressed: () => _showMediaPickerDialog(),
+                  icon: const Icon(Icons.photo_library),
+                  label: Text(
+                    "Select from Gallery",
+                    style: theme.textTheme.bodySmall,
+                  ),
                 ),
                 ElevatedButton.icon(
-                  onPressed: () {},
-                  icon: const Icon(Icons.camera),
-                  label: const Text("Camera"),
+                  onPressed: () => _showMediaPickerDialog(),
+                  icon: const Icon(Icons.camera_alt),
+                  label: Text(
+                    "Take a Photo",
+                    style: theme.textTheme.bodySmall,
+                  ),
                 ),
               ],
             ),
+            const SizedBox(height: 16),
+            _buildMediaPreview(),
             const SizedBox(height: 32),
             _buildCostAndOrderButton(theme),
           ],
@@ -306,64 +355,58 @@ class _BookingScreenState extends State<BookingScreen>
     );
   }
 
-  Widget _buildCostAndOrderButton(ThemeData theme) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("Total Cost", style: theme.textTheme.bodyMedium),
-            Text(
-              "Rp0",
-              style: theme.textTheme.titleMedium!
-                  .copyWith(fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
-        ElevatedButton(
-          onPressed: () {},
-          style: ElevatedButton.styleFrom(
-            backgroundColor: theme.primaryColor,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30),
-            ),
-          ),
-          child: const Text("Order"),
-        ),
-      ],
-    );
+  Widget _buildMediaPreview() {
+    if (_images!.isNotEmpty || _videos!.isNotEmpty) {
+      return Column(
+        children: [
+          if (_images!.isNotEmpty)
+            ..._images!.map((image) => Image.file(File(image.path))),
+          if (_videos!.isNotEmpty)
+            ..._videos!.map((video) => Text('Video: ${video.path}')),
+        ],
+      );
+    }
+    return const Text("No media selected.");
   }
 
-  Widget _serviceOption(BuildContext context,
-      {required IconData icon,
-      required String label,
-      required bool isSelected,
-      required VoidCallback onTap}) {
-    final theme = Theme.of(context);
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        width: MediaQuery.of(context).size.width * 0.4,
-        padding: const EdgeInsets.symmetric(vertical: 16),
-        decoration: BoxDecoration(
-          color: isSelected ? const Color(0xFFFECE2E) : Colors.white,
-          borderRadius: BorderRadius.circular(10),
-          border:
-              Border.all(color: isSelected ? theme.primaryColor : Colors.grey),
+  Widget _buildCostAndOrderButton(ThemeData theme) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Divider(
+          thickness: 1,
+          color: Colors.black26,
         ),
-        child: Column(
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Icon(icon, color: theme.primaryColor, size: 40),
-            const SizedBox(height: 8),
-            Text(
-              label,
-              textAlign: TextAlign.center,
-              style: theme.textTheme.labelLarge,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Total Cost", style: theme.textTheme.bodyMedium),
+                Text(
+                  "Rp0",
+                  style: theme.textTheme.titleMedium!.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: theme.primaryColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+              child: Text("Order",
+                  style: theme.textTheme.titleMedium!
+                      .copyWith(fontWeight: FontWeight.bold)),
             ),
           ],
         ),
-      ),
+      ],
     );
   }
 }
---
+++ lib/widgets/service_options.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class ServiceOptions extends StatelessWidget {
+  final String selectedService;
+  final Function(String) onServiceSelected;
+
+  const ServiceOptions({
+    Key? key,
+    required this.selectedService,
+    required this.onServiceSelected,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _serviceOption(
+          context,
+          imagePath: 'assets/icons/recharge_freon.png',
+          label: "Recharge Freon",
+          isSelected: selectedService == "Recharge Freon",
+          onTap: () => onServiceSelected("Recharge Freon"),
+        ),
+        _serviceOption(
+          context,
+          imagePath: 'assets/icons/cleaning_up.png',
+          label: "Cleaning Up",
+          isSelected: selectedService == "Cleaning Up",
+          onTap: () => onServiceSelected("Cleaning Up"),
+        ),
+      ],
+    );
+  }
+
+  Widget _serviceOption(
+    BuildContext context, {
+    required String imagePath,
+    required String label,
+    required bool isSelected,
+    required VoidCallback onTap,
+  }) {
+    final theme = Theme.of(context);
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.4,
+        padding: const EdgeInsets.symmetric(vertical: 16),
+        decoration: BoxDecoration(
+          color: isSelected ? theme.primaryColor : Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          border:
+              Border.all(color: isSelected ? theme.primaryColor : Colors.grey),
+        ),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset(
+                imagePath,
+                width: 31,
+                height: 40,
+                fit: BoxFit.contain,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              label,
+              textAlign: TextAlign.center,
+              style: theme.textTheme.labelLarge,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
