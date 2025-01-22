555b1961569779f3a87db97ef68446538de08e42
EdrickLionard <148632769+EdrickLionard@users.noreply.github.com>
2025-01-22 11:55:06 +0800

    education & profile page

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,12 +1,30 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:front_end/widgets/navbar.dart' as navbar;
-import 'package:front_end/widgets/terms_conditions.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(),
+      ),
+      home: const ProfilePage(),
+    );
+  }
+}
 
 class ProfilePage extends StatefulWidget {
-  const ProfilePage({super.key});
+  const ProfilePage({Key? key}) : super(key: key);
 
   @override
   State<ProfilePage> createState() => _ProfilePageState();
@@ -14,7 +32,7 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   bool isPasswordVisible = false;
-  String userPassword = "password"; // Simulasi password dari data user
+  String userPassword = "makkkkmintakiko"; // Simulasi password dari data user
   File? _imageFile; // File untuk menyimpan gambar yang dipilih
   final ImagePicker _picker = ImagePicker(); // Inisialisasi ImagePicker
 
@@ -74,220 +92,226 @@ class _ProfilePageState extends State<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-
-    return navbar.BottomNavBar(
-      currentIndex: 4,
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Header
-            Container(
-              padding: const EdgeInsets.only(top: 20, left: 16, bottom: 10),
-              child: Text(
-                "Profile",
-                style: theme.textTheme.headlineSmall,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Header
+          Container(
+            padding: const EdgeInsets.only(top: 20, left: 16, bottom: 10),
+            child: Text(
+              "Profile",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w700,
+                color: Colors.black,
               ),
             ),
+          ),
 
-            // Divider
-            const Divider(
-              color: Colors.black12,
-              height: 1,
-              thickness: 1,
-            ),
+          // Divider
+          const Divider(
+            color: Colors.black12,
+            height: 1,
+            thickness: 1,
+          ),
 
-            // Profile Picture Section with Gradient Background
-            Container(
-              width: double.infinity,
-              padding: const EdgeInsets.only(top: 20, bottom: 20),
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Color(0xFFFFEA4F),
-                    Colors.white,
-                  ],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                ),
+          // Profile Picture Section with Gradient Background
+          Container(
+            width: double
+                .infinity, // Pastikan background gradient mencakup seluruh lebar layar
+            padding: const EdgeInsets.only(top: 20, bottom: 20),
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 255, 234, 79),
+                  Colors.white,
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                stops: [0.2, 1.0], // Feather lebih panjang untuk gradasi halus
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Stack(
-                    children: [
-                      CircleAvatar(
-                        radius: 50,
-                        backgroundColor: Colors.white,
-                        backgroundImage:
-                            _imageFile != null ? FileImage(_imageFile!) : null,
-                        child: _imageFile == null
-                            ? const Icon(
-                                Icons.person,
-                                size: 60,
-                                color: Color(0xFFFECE2E),
-                              )
-                            : null,
+            ),
+            child: Column(
+              mainAxisAlignment:
+                  MainAxisAlignment.center, // Pusatkan secara vertikal
+              crossAxisAlignment:
+                  CrossAxisAlignment.center, // Pusatkan secara horizontal
+              children: [
+                Stack(
+                  children: [
+                    const CircleAvatar(
+                      radius: 50,
+                      backgroundColor: Colors.white,
+                      child: Icon(
+                        Icons.person,
+                        size: 60,
+                        color: Color(0xFFFFD54F),
                       ),
-                      Positioned(
-                        bottom: 0,
-                        right: 0,
-                        child: GestureDetector(
-                          onTap: _showImagePickerOptions,
-                          child: const CircleAvatar(
-                            backgroundColor: Colors.black,
-                            radius: 16,
-                            child: Icon(
-                              Icons.edit,
-                              size: 16,
-                              color: Colors.white,
-                            ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: GestureDetector(
+                        onTap: _showImagePickerOptions,
+                        child: CircleAvatar(
+                          backgroundColor: Colors.black,
+                          radius: 16,
+                          child: const Icon(
+                            Icons.edit,
+                            size: 16,
+                            color: Colors.white,
                           ),
                         ),
                       ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  "chaidenfoanto",
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  "+6289731469826",
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          // Email Section
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black12),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "E-mail",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Row(
+                    children: [
+                      const Icon(Icons.email, color: Colors.black),
+                      const SizedBox(width: 16),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "E-mail address",
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.black54,
+                            ),
+                          ),
+                          Text(
+                            "chaidenfoanto@gmail.com",
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
                     ],
                   ),
-                  const SizedBox(height: 8),
-                  Text(
-                    "chaidenfoanto",
-                    style: theme.textTheme.titleLarge?.copyWith(fontWeight: FontWeight.w600),
-                  ),
-                  const SizedBox(height: 4),
-                  Text(
-                    "+6289731469826",
-                    style: theme.textTheme.titleMedium?.copyWith(fontWeight: FontWeight.w600),
-                  ),
-                ],
-              ),
-            ),
-
-            // Email Section
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Container(
-                padding: const EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black12),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "E-mail",
-                      style: theme.textTheme.bodyLarge?.copyWith(
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    Row(
-                      children: [
-                        const Icon(Icons.email, color: Colors.black),
-                        const SizedBox(width: 16),
-                        Column(
+                  const SizedBox(height: 16),
+                  Row(
+                    children: [
+                      const Icon(Icons.lock, color: Colors.black),
+                      const SizedBox(width: 16),
+                      Expanded(
+                        child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Text(
-                              "E-mail address",
-                              style: theme.textTheme.bodyMedium?.copyWith(
+                              "Password",
+                              style: TextStyle(
+                                fontSize: 14,
                                 color: Colors.black54,
                               ),
                             ),
                             Text(
-                              "chaidenfoanto@gmail.com",
-                              style: theme.textTheme.bodyMedium,
+                              isPasswordVisible ? userPassword : "************",
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                              ),
                             ),
                           ],
                         ),
-                      ],
-                    ),
-                    const SizedBox(height: 16),
-                    Row(
-                      children: [
-                        const Icon(Icons.lock, color: Colors.black),
-                        const SizedBox(width: 16),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Password",
-                                style: theme.textTheme.bodyMedium?.copyWith(
-                                  color: Colors.black54,
-                                ),
-                              ),
-                              Text(
-                                isPasswordVisible
-                                    ? userPassword
-                                    : "************",
-                                style: theme.textTheme.bodyMedium,
-                              ),
-                            ],
-                          ),
+                      ),
+                      GestureDetector(
+                        onTapDown: (_) {
+                          setState(() {
+                            isPasswordVisible = true;
+                          });
+                        },
+                        onTapUp: (_) {
+                          setState(() {
+                            isPasswordVisible = false;
+                          });
+                        },
+                        onTapCancel: () {
+                          setState(() {
+                            isPasswordVisible = false;
+                          });
+                        },
+                        child: Icon(
+                          isPasswordVisible
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                          color: Colors.black54,
                         ),
-                        GestureDetector(
-                          onTapDown: (_) {
-                            setState(() {
-                              isPasswordVisible = true;
-                            });
-                          },
-                          onTapUp: (_) {
-                            setState(() {
-                              isPasswordVisible = false;
-                            });
-                          },
-                          onTapCancel: () {
-                            setState(() {
-                              isPasswordVisible = false;
-                            });
-                          },
-                          child: Icon(
-                            isPasswordVisible
-                                ? Icons.visibility
-                                : Icons.visibility_off,
-                            color: Colors.black54,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
+          ),
 
-            const SizedBox(height: 16),
+          const SizedBox(height: 16),
 
-            // Terms & Conditions and Contact Us
-            ListTile(
-              leading: const Icon(Icons.description, color: Colors.black),
-              title: Text(
-                "Terms & Conditions",
-                style: theme.textTheme.bodyLarge?.copyWith(
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              trailing: const Icon(Icons.arrow_forward_ios, size: 16),
-              onTap: () async {
-                showDialog(
-                  context: context,
-                  builder: (context) => const TermsConditionsDialog(),
-                );
-              },
+          // Terms & Conditions and Contact Us
+          ListTile(
+            leading: const Icon(Icons.description, color: Colors.black),
+            title: Text(
+              "Terms & Conditions",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
             ),
-            ListTile(
-              leading: const Icon(Icons.phone, color: Colors.black),
-              title: Text(
-                "Contact us",
-                style: theme.textTheme.bodyLarge?.copyWith(
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              trailing: const Icon(Icons.arrow_forward_ios, size: 16),
+            trailing: const Icon(Icons.arrow_forward_ios, size: 16),
+          ),
+          ListTile(
+            leading: const Icon(Icons.phone, color: Colors.black),
+            title: Text(
+              "Contact us",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
             ),
-          ],
-        ),
+            trailing: const Icon(Icons.arrow_forward_ios, size: 16),
+          ),
+        ],
       ),
     );
   }
